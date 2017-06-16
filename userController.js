f4639d81fe6e5f2c612b29cb0a264ce3023b95c6
David <d.vinternatt@gmail.com>
2017-06-16 19:21:06 +0100

    Coding improvements

--- userController.js
+++ userController.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var fs = require('fs');
+var userObj = require('./users/users.json');
 
 /*
  * Esta función busca en el archivo JSON un usuario con el atributo isActive = false,
@@ -8,11 +9,9 @@ var fs = require('fs');
  * a CoovaChilli... Si no hay usuarios activos pone el atributo userAndPass a null.
  * */
 exports.getInactiveUser = function() {
-    var jsonContents = fs.readFileSync("./users/users.json");
-    var usersList = JSON.parse(jsonContents);
-    //console.log(usersList.users[1].username); //Esto me daría CORRECTAMENTE el username del segundo elemento del JSON
-    for (var i =0; i<usersList.users.length; i++){
-        if (!usersList.users[i].isActive){
+    //console.log(userObj.users[1].username); //Esto me daría CORRECTAMENTE el username del segundo elemento del JSON
+    for (var i =0; i<userObj.users.length; i++){
+        if (!userObj.users[i].isActive){
             setUserActive(i);
             return prepareToConnect(i);
         }
@@ -24,27 +23,25 @@ exports.getInactiveUser = function() {
  * Lee el JSON de usuarios, marca el usuario como activo y lo guarda en el archivo.
  * */
 function setUserActive(userId) {
-    var jsonContents = fs.readFileSync("./users/users.json");
-    var usersList = JSON.parse(jsonContents);
-    usersList.users[userId].isActive = true;
-    fs.writeFileSync("./users/users.json", JSON.stringify(usersList, null, 2));
+    userObj.users[userId].isActive = true;
+    writeUsersFile(userObj);
 }
 /*
  * Lee el JSON de usuarios, marca el usuario como inactivo y lo guarda en el archivo.
  * (aún sin uso, para la futura desconexión de usuarios...)
  * */
 function setUserInactive(userId) {
-    var jsonContents = fs.readFileSync("./users/users.json");
-    var usersList = JSON.parse(jsonContents);
-    usersList.users[userId].isActive = false;
-    fs.writeFileSync("./users/users.json", JSON.stringify(usersList, null, 2));
+    userObj.users[userId].isActive = false;
+    writeUsersFile(userObj);
 }
 /*
  * Lee el JSON de usuarios y extrae el nombre de usuario y contraseña del usuario con la ID seleccionada
  * para escribirlos en el JSON de usuario activo, para que lo use el script front-end...
  * */
 function prepareToConnect(userId) {
-    var jsonContents = fs.readFileSync("./users/users.json");
-    var usersList = JSON.parse(jsonContents);
-    return [usersList.users[userId].username, usersList.users[userId].password];
+    return [userObj.users[userId].username, userObj.users[userId].password];
+}
+
+function writeUsersFile(userJSON){
+    fs.writeFileSync("./users/users.json", JSON.stringify(userJSON, null, 2));
 }
