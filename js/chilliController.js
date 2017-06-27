2d1c13255da6860f062ad5fedfa91aeed4f2ce17
David <d.vinternatt@gmail.com>
2017-06-27 09:49:09 +0100

    Separated chilliController javascript from index.html

---
+++ js/chilliController.js
@@ -0,0 +1,25 @@
+// The included script ChilliLibrary.js creates a global chilliController object
+// then define event handler functions
+chilliController.onError  = handleErrors;
+chilliController.onUpdate = updateUI ;
+
+//FUNCIÓN PARA CONECTARSE A CHILLI //TODO mudar a script.js y probar si funciona
+function connect(username, password){
+    console.log('Conectando...');
+    if (username == "" || password == "") // ELABORAR
+        console.log('Algo va mal... ¿Usuarios completos? User: '+username+'. Pass: '+password+'.');
+    chilliController.logon(username, password);
+}
+
+// when the reply is ready, this handler function is called
+function updateUI( cmd ) {
+    console.log('You called the method ' + cmd +
+        '\n Your current state is = ' + chilliController.clientState);
+}
+
+// If an error occurs, this handler will be called instead
+function handleErrors ( code ) {
+    console.log( 'The last contact with the Controller failed. Error code = ' + code );
+}
+//  finally, get current state
+chilliController.refresh();
