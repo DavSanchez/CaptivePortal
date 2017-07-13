3c440c1f4f47fd23490bf6fad3b1aa9a2a927b53
DavSanchez <d.vinternatt@gmail.com>
2017-07-14 00:08:02 +0100

    HTTPS deployment?

--- nodeserver.js
+++ nodeserver.js
@@ -133,10 +133,11 @@ function setCreds(){
  * Now that we have everything set up and the route to handle the uploads in place,
  * all we need to do it start our NodeJS server and start processing uploads!
  * */
-// var server = app.listen(3000, function(){
-//     console.log('Server listening on port 3000');
-// });
+var server = app.listen(3000, function(){
+    console.log('Server listening on port 3000');
+});
 
+/*
 const options = {
     key: fs.readFileSync('ssl/server.key'),
     cert: fs.readFileSync('ssl/server.crt')
@@ -144,10 +145,10 @@ const options = {
     // passphrase: 'sample'
 };
 
-// http.createServer(app).listen(8080, function(){
-//     console.log('HTTP server listening on port 8080');
-// });
+http.createServer(app).listen(8080, function(){
+    console.log('HTTP server listening on port 8080');
+});
 
 https.createServer(options, app).listen(4430, function(){
     console.log('HTTPS server listening on port 4430');
-});
+});*/
