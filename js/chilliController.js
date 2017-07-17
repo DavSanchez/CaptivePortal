4d604360ade5233da5f4696745d8931abc868d83
David <d.vinternatt@gmail.com>
2017-07-17 11:18:21 +0100

    SSL Adjustments 2

--- js/chilliController.js
+++ js/chilliController.js
@@ -5,7 +5,6 @@ chilliController.onUpdate = updateUI ;
 
 // AJUSTES TEMPORALES PARA SSL (HTTPS)
 chilliController.ssl = true;
-chilliController.port = 3990;
 
 //FUNCIÓN PARA CONECTARSE A CHILLI
 function connect(username, password){
