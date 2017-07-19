2eda1b1e7ed54ddcdf3b637d030cd16c532afbf5
DavSanchez <d.vinternatt@gmail.com>
2017-07-19 01:39:36 +0100

    Added info in comments

--- nodeserver.js
+++ nodeserver.js
@@ -1,6 +1,7 @@
 /*
 ESTAMOS USANDO PM2!!
 https://www.digitalocean.com/community/tutorials/how-to-set-up-a-node-js-application-for-production-on-ubuntu-16-04
+http://pm2.keymetrics.io/docs/usage/startup/
  */
 
 /*
