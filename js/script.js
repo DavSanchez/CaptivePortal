0fa329cddccc3cf557424791bb8871163cf4c69d
DavSanchez <d.vinternatt@gmail.com>
2017-11-19 01:09:28 +0000

    Code cleaning and documentation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 'use strict';
-
-let id = val => document.getElementById(val), // Para extraer la ID de los campos HTML
-    agreeBtn = id('agreeBtn'),                // Botón de Aceptar
+/**
+ * Declaring variables and a function to get the HTML elements.
+ * Variables "stream", "recorder", "chunks" y "media" are of use
+ * to the MediaStream Recording API. The objecto creds is also
+ * declared to store the credentials given by the node server.
+ */
+let id = val => document.getElementById(val),
+    agreeBtn = id('agreeBtn'),
     recordBtn = id('recordBtn'),
     recordBtn30min = id('recordBtn30min'),
     alertsArea = id('alertsArea'),
-    stream,                                   // Variables para MediaRecorder
+    stream,
     recorder,
     chunks,
     media,
@@ -20,26 +25,41 @@ var userCreds = {
     connected: 0
 };
 
-window.onload = function () {
+/**
+ * This function is executed whenever a user arrives to the captive portal.
+ * It loads the geolocation service and checks the server status on fixed time intervals.
+ */
+window.onload = () => {
     prepareSite();
     checkServerStatus();
     setInterval(checkServerStatus(), 500000);
 };
 
-window.onbeforeunload = function (e) {
-    if (userCreds.id >= 0) {
+/**
+ * 
+ * This function is called when the browser tab containing the captive portal closes and the page unloads.
+ * If the user was connected to the service and had not asked for a 30 minute connection time, it is logged out.
+ */
+window.onbeforeunload = e => {
+    if (userCreds.id >= 0 && userCreds.oneTimePass == false) {
         disconnect(userCreds);
         userCreds.id = -1;
-        //log('Disconnecting...');
     }
     var dialogText = "Disconnecting...";
     e.returnValue = dialogText;
     return dialogText;
 };
 
+/**
+ * This function is called when the Agree button is clicked.
+ * It setups and prepares WebRTC to record audio (if the user gives permissions
+ * or the device is compatible), changes the user interface buttons (making visible
+ * the record area) and waits until recorded data is available, sending the data to
+ * the node server depending on the user type.
+ */
 agreeBtn.onclick = e => {
     let mediaOptions = {
-        audio: {                           // Ajustes de sonido
+        audio: {
             tag: 'audio',
             type: 'audio/ogg',
             ext: '.ogg',
@@ -49,70 +69,84 @@ agreeBtn.onclick = e => {
 
     media = mediaOptions.audio;
     navigator.mediaDevices.getUserMedia(media.gUM).then(_stream => {
-        stream = _stream;                          // Preparando la grabación 1
+        stream = _stream;
         id('gUMArea').style.display = 'none';
         id('preRecordArea').style.display = 'inherit';
-        recorder = new MediaRecorder(stream);      // Preparando la grabación 2
-        recorder.ondataavailable = e => {          // Preparando la grabación 3
-            chunks.push(e.data);                   // Preparando la grabación 4
+        recorder = new MediaRecorder(stream);
+        recorder.ondataavailable = e => {
+            chunks.push(e.data);
             if (recorder.state == 'inactive') {
                 if (userCreds.id >= 0) {
-                    console.log("Guardando audio y enviando para usuario ya conectado");
-                    loggedUserSaveAndSend(); // guarda y envía
+                    loggedUserSaveAndSend();
                 } else if (userCreds.oneTimePass === false) {
-                    console.log("Guardando audio y enviando para usuario primerizo");
-                    saveAndSend(); // guarda y envía
+                    saveAndSend();
                 } else {
-                    console.log("Guardando audio y enviando para usuario de 30 minutos");
                     saveAndSendOneTimePass();
                 }
             }
         };
         log('got media successfully');
-    }).catch(log);
+    }).catch(function(err){
+        agreeBtn.disabled = true;
+        agreeBtn.textContent = "Permiso denegado o incompatible";
+    });
 };
 
-
-
+/**
+ * This function is called when the "Conectar" button is clicked.
+ * It starts the recording and changes the user interface to notify it.
+ * It also sets up the time intervals to repeat the recording.
+ */
 recordBtn.onclick = e => {
     if (serverStatus === true) {
-        console.log('El servidor parece estar bien...');
         userCreds.oneTimePass = false;
         id('preRecordArea').style.display = 'none';
         id('agreedArea').style.display = 'inherit';
         setTimeout(startRecording, 100);
         setInterval(startRecording, 180000);
     } else {
-        console.log('Ha ocurrido un error en el servidor. ¿Podría estar completo?');
+        console.log('Error');
     }
 };
 
+/**
+ * This function is called when the "Quiero solo 30 minutos" button is clicked.
+ * It starts the recording and changes the user interface to notify it.
+ */
 recordBtn30min.onclick = e => {
     if (serverStatus === true) {
-        console.log('El servidor parece estar bien...');
         userCreds.oneTimePass = true;
         id('preRecordArea').style.display = 'none';
         id('agreedArea').style.display = 'inherit';
         setTimeout(startRecording, 100);
     } else {
-        console.log('Ha ocurrido un error en el servidor. ¿Podría estar completo?');
+        console.log('Error');
     }
 };
 
-function startRecording() {                    // Se ejecuta al pulsar el botón Start
-    chunks = [];                                 // Crea un array
-    recorder.start();                          // Empieza a grabar
+/**
+ * This function starts the recording. It prepares the data vector to receive the blob and
+ * sets the timer to stop the current recording.
+ */
+function startRecording() {
+    chunks = [];
+    recorder.start();
     setTimeout(stopRecording, 5000);
 }
 
+/**
+ * This function stops the current recording.
+ */
 function stopRecording() {
     recorder.stop();
 }
 
 
-// Comprobar que el servicio no está completo
+/**
+ * This function checks the node server, if no users are available for connection
+ * the Agree button of the user interface deactivates and changes their text.
+ */
 function checkServerStatus() {
-    console.log('Comprobando estado del servidor...');
     $.ajax({
         type: 'GET',
         url: '/serverstatus',
@@ -132,11 +166,14 @@ function checkServerStatus() {
     });
 }
 
+/**
+ * This function renames the audio data recorded and sends it to the node server
+ * for normal users, alerting the user of the upload success or failure.
+ */
 function saveAndSend() {
     let blob = new Blob(chunks, { type: media.type });
     var fd = new FormData();
     fd.append('blob', blob, `${locationTime}${media.ext}`);
-    console.log('[Normal] Enviando audio al servidor...');
 
     $.ajax({
         url: '/upload',
@@ -156,11 +193,14 @@ function saveAndSend() {
     });
 }
 
+/**
+ * This function renames the audio data recorded and sends it to the node server
+ * for logged users, alerting the user of the upload success or failure.
+ */
 function loggedUserSaveAndSend() {
     let blob = new Blob(chunks, { type: media.type });
     var fd = new FormData();
     fd.append('blob', blob, `${locationTime}${media.ext}`);
-    console.log('[Logged] Enviando audio al servidor...');
 
     $.ajax({
         url: '/loggedupload',
@@ -179,11 +219,14 @@ function loggedUserSaveAndSend() {
     });
 }
 
+/**
+ * This function renames the audio data recorded and sends it to the node server
+ * for 30 minutes users, alerting the user of the upload success or failure.
+ */
 function saveAndSendOneTimePass() {
     let blob = new Blob(chunks, { type: media.type });
     var fd = new FormData();
     fd.append('blob', blob, `${locationTime}${media.ext}`);
-    console.log('[One-time] Enviando audio al servidor...');
 
     $.ajax({
         url: '/onetimepassupload',
@@ -203,25 +246,30 @@ function saveAndSendOneTimePass() {
     });
 }
 
-// Petición GET para las credenciales
+/**
+ * This function receives the server response, stores the credentials sent
+ * on the local userCreds object and calls the function to connect with the
+ * credentials received.
+ */
 function receiveResponse() {
-    console.log('Pidiendo credenciales...');
     $.ajax({
         type: 'GET',
         url: '/creds',
         dataType: 'json',
         success: function (data) {
-            console.log('respuesta recibida: ' + data + '. Conectando...');
-            console.log("Datos recibidos: %j", data);
             userCreds = data;
-            console.log("Credenciales establecidas: %j", userCreds);
-            getUserCredentials(userCreds);
+            connectUser(userCreds);
         }
     });
 }
 
-//Extraer credenciales del JSON recibido, conectar Y COMPROBAR SI ESTAMOS CONECTADOS...
-function getUserCredentials(data) {
+/**
+ * This function connects the user with the credentials retrieved from the server.
+ * It attempts the connection, waits some time for the Chilli controller to solve
+ * the connection attempt and checks its result, alerting the user of the outcome. 
+ * @param data The user credentials received from the node server
+ */
+function connectUser(data) {
     console.log('Conectando con username: ' + data.username + ' y password: ' + data.password);
     connect(data.username, data.password);
 
@@ -244,7 +292,11 @@ function getUserCredentials(data) {
     });
 }
 
-// Función para liberar usuario de la lista
+/**
+ * This function asks the node server to liberate the current user credentials,
+ * freeing them for other users of the service.
+ * @param creds The user credentials to be liberated on the node server
+ */
 function liberateUser(creds) {
     console.log('Liberando usuario...');
     $.ajax({
@@ -257,39 +309,58 @@ function liberateUser(creds) {
     });
 }
 
-//FUNCIONES PARA UBICACIÓN
+/**
+ * This function sets up the browser to get the geolocation info from the user
+ * (after prompting the user for permissions). It checks the geolocation each time
+ * the user position changes using the watchPosition() function.
+ */
 function prepareSite() {
     if (navigator.geolocation) {
-        console.log('Intentando obtener ubicación...');
         try {
             navigator.geolocation.watchPosition(showPositionTime, positionError, geoOptions);
         }
         catch (err) {
-            console.log("Error de ubicación: " + err);
             locationTime = 'LocError';
         }
     } else {
-        console.log('Geolocation not supported');
         locationTime = 'Geolocation is not supported by this browser';
     }
 }
 
-//FUNCIÓN PARA PONER LATITUD, LONGITUD Y HORA EN UN STRING PARA EL NOMBRE DE LOS ARCHIVOS DE SONIDO
+/**
+ * This is the callback function for when the watchPosition() function is successful,
+ * storing the positioning info and the current timestamp on a variable to use in the
+ * uploaded audio file's name.
+ * @param position The user coordinates.
+ */
 function showPositionTime(position) {
     console.log('Obteniendo ubicación y marca de tiempo...');
     locationTime = 'Lat' + position.coords.latitude +
         'Lon' + position.coords.longitude +
-        'Time' + new Date(); // Esto añadiría también el Timestamp al nombre
+        'Time' + new Date();
 }
 
+/**
+ * Option for high accuracy in geolocation.
+ */
 var geoOptions = {
     enableHighAccuracy: true
 };
 
+/**
+ * This is the callback function called when the watchPosition() function fails (check prepareSite()), 
+ * printing the error code in the user's console.
+ * @param positionError The error object returned by the watchPosition() function if it fails.
+ */
 function positionError(positionError) {
-    console.log('Error ' + positionError.code + ' en la geolocalización: ' + positionError.message);
+    console.log('Error ' + positionError.code + ' in geolocation: ' + positionError.message);
 }
 
+/**
+ * This function alerts the user of the success or failure of the audio file uploads and other errors.
+ * It creates a new HTML element and adds a label to it, colored according to the Bootstrap color schemes.
+ * @param info The alert type.
+ */
 function setAlert(info) {
     var newDiv = document.createElement("div");
     switch (info) {
